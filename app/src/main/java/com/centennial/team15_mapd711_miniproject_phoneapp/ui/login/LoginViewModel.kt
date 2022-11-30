db4ece372efbade59e5b05cd785e445a94bb1c53
jovichenmcintyrecentenial <jchenmci@my.centennialcollege.ca>
2022-11-30 14:15:06 -0500

    added connection to firebase store

--- app/src/main/java/com/centennial/team15_mapd711_miniproject_phoneapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/centennial/team15_mapd711_miniproject_phoneapp/ui/login/LoginViewModel.kt
@@ -1,6 +1,14 @@
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class LoginViewModel: ViewModel() {
 
@@ -9,9 +17,26 @@ class LoginViewModel: ViewModel() {
     }
 
     fun getUser(context: Context, username:String, password:String){
-//        CoroutineScope(Dispatchers.IO).launch {
+        FirebaseApp.initializeApp(context)
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+
+        CoroutineScope(Dispatchers.IO).launch {
 //            liveCustomerData.postValue( CustomerRepository.passwordCheck(context, username, password))
-//        }
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("TAG", "Error adding document", e)
+                }
+        }
     }
 
 
